3a36d02f0213da498c6d5e86c536fd64226ef2b5
Bihart <sanket.bihari@gmail.com>
2022-11-13 17:27:24 -0500

    [fix] Remove unnecessary files.
    Signed-off-by: Bihart <sanket.bihari@gmail.com>

--- lunari-core/target/debug/deps/lunari_core-36996670c0e1980d.d
+++
@@ -1,5 +0,0 @@
-/home/cesar/Development/lunari/lunari-core/target/debug/deps/lunari_core-36996670c0e1980d.rmeta: src/main.rs
-
-/home/cesar/Development/lunari/lunari-core/target/debug/deps/lunari_core-36996670c0e1980d.d: src/main.rs
-
-src/main.rs:
