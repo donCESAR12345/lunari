26c61de367d61baf7968ad4cc8ff884ae3ccf8f5
donCESAR12345 <sprit152009@gmail.com>
2022-11-13 17:14:42 -0500

    Lunari first commit
    Signed-off-by: donCESAR12345 <sprit152009@gmail.com>

---
+++ lunari-core/target/debug/deps/lunari_core-3a71cbdb51c8a1cf.d
@@ -0,0 +1,5 @@
+/home/cesar/Development/lunari/lunari-core/target/debug/deps/lunari_core-3a71cbdb51c8a1cf.rmeta: src/main.rs
+
+/home/cesar/Development/lunari/lunari-core/target/debug/deps/lunari_core-3a71cbdb51c8a1cf.d: src/main.rs
+
+src/main.rs:
