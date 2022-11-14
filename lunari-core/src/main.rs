bbe9ba5aec4c52d8e81260e0b7a42ec4565c6fb6
donCESAR12345 <sprit152009@gmail.com>
2022-11-13 21:03:28 -0500

    Fixed format and refactored a bit
    Signed-off-by: donCESAR12345 <sprit152009@gmail.com>

--- lunari-core/src/main.rs
+++ lunari-core/src/main.rs
@@ -1,17 +1,20 @@
 use chrono::NaiveDateTime;
 use clap::Parser;
 
-const PHASES: [&str; 8] = [
+// Fases de la luna
+const PHASES: [&str; 9] = [
     "luna nueva",
     "luna creciente",
     "primer cuarto",
     "luna menguante",
     "luna llena",
     "gibosa menguante",
-    "último cuarto",
+    "tercer cuarto",
     "creciente menguante",
+    "luna nueva",
 ];
 
+// Intervalos de edad de la luna
 const AGES: [f64; 10] = [
     0.0,
     1.0,
@@ -25,6 +28,10 @@ const AGES: [f64; 10] = [
     29.53058770576,
 ];
 
+// Constantes del período lunar
+const LUNAR_DAYS: f64 = 29.53058770576;
+const LUNAR_SECS: f64 = LUNAR_DAYS * (24.0 * 60.0 * 60.0);
+
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
@@ -33,33 +40,32 @@ struct Args {
 }
 
 fn main() {
-    // Constantes
-    let lunardays: f64 = 29.53058770576;
-    let lunarsecs: f64 = lunardays * (24.0 * 60.0 * 60.0);
-    let args = Args::parse();
     let new_2000: f64 = NaiveDateTime::parse_from_str("2000-01-06 18:14", "%Y-%m-%d %H:%M")
         .unwrap()
         .timestamp() as f64;
+    let args = Args::parse();
     let date = NaiveDateTime::parse_from_str(&args.date, "%Y-%m-%d %H:%M").unwrap();
     let date_timestamp: f64 = date.timestamp() as f64;
     let total_secs: f64 = date_timestamp - new_2000;
-    let mut current_secs: f64 = total_secs % lunarsecs;
+    let mut current_secs: f64 = total_secs % LUNAR_SECS;
 
     if current_secs < 0.0 {
-        current_secs += lunarsecs;
+        current_secs += LUNAR_SECS;
     }
     // Calculate the fraction of the moon cycle
-    let currentfrac: f64 = current_secs / lunarsecs;
+    let currentfrac: f64 = current_secs / LUNAR_SECS;
 
     // Calculate days in current cycle (moon age)
-    let currentdays = currentfrac * lunardays;
+    let currentdays = currentfrac * LUNAR_DAYS;
 
-    let mut index: usize = 0;
+    let mut phase: &str = "";
 
+    // Encuentra el intervalo en el que se encuentra
     for i in 0..AGES.len() - 1 {
-        if currentdays > AGES[i] && currentdays < AGES[i + 1] {}
+        if currentdays > AGES[i] && currentdays < AGES[i + 1] {
+            phase = PHASES[i];
+        }
     }
 
-    let phase = PHASES[index];
-    println!("Para la fecha {} la luna estará en {}", date, phase);
+    println!("Para la fecha {} la luna estará en {}.", date, phase);
 }
